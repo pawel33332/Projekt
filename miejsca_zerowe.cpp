4ba96682c58e065073e9b91ed074b118d9ea6315
pawel33332 <pawel33332@gmail.com>
2021-10-26 12:56:52 +0200

    wersja 1.2

--- miejsca_zerowe.cpp
+++ miejsca_zerowe.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+
 using namespace std;
 
 
 int main()
 {
-	dobule a, b;
-	cout<<"Podaj wspó³czynnik a: ";
+	double a, b;
+	cout<<"Podaj wspolczynnik a: ";
 	cin>>a;
 	cout<<"Podaj wyraz b: ";
 	cin>>b;
-	cout<<"Miejsce zerowe funkcji wynosi: "<<-b/a<<endl;
+	if(a!=0)
+	{
+		cout<<"Miejsce zerowe funkcji wynosi: "<<-b/a<<endl;
+	}
+	else {
+		if(b==0)
+		{
+		cout<<"Funkcja ma nieskonczenie wiele miejsc zerowych"<<endl;
+	
+		}
+		else {
+			cout<<"Funkcja nie ma miejsc zerowych"<<endl;
+		}
+	}
+	
 	
 }
