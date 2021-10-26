4bc1cdde28eab28c73ccffe2736840af3cf5f530
pawel33332 <pawel33332@gmail.com>
2021-10-26 12:12:22 +0200

    pierwsza wersja

---
+++ miejsca_zerowe.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	dobule a, b;
+	cout<<"Podaj wspó³czynnik a: ";
+	cin>>a;
+	cout<<"Podaj wyraz b: ";
+	cin>>b;
+	cout<<"Miejsce zerowe funkcji wynosi: "<<-b/a<<endl;
+	
+}
