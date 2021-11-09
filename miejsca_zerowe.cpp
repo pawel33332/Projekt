ba9b40e243b39eaacc3ff07ff0dd181198f0340e
pawel33332 <pawel33332@gmail.com>
2021-11-09 12:58:39 +0100

    wersja 2.5 dodanie funkcji kanonicznej

--- miejsca_zerowe.cpp
+++ miejsca_zerowe.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main()
 {
-	double a, b,c ;
+	double a, b,c,p,q;
 	int funkcja;
-	cout<<"Czy chcesz obliczyc miejsca zerowe funckji kwadratowej (wpisz 1) czy funkcje liniowa (wpisz 2)"<<endl;
+	cout<<"Czy chcesz obliczyc miejsca zerowe funckji kwadratowej (wpisz 1) funkcji liniowej (wpisz 2) czy na podstawie funkcji kanonicznej (wpisz 3)"<<endl;
 	cin>>funkcja;
 	
 	if(funkcja==1)
@@ -73,6 +73,50 @@ int main()
 			cout<<"Funkcja nie ma miejsc zerowych "<<endl;
 		}
 	}	
+	} else if(funkcja==3)
+	{
+		cout<<"Wybrales obliczanie miejsc zerowych na podstawie funkcji kanonicznej"<<endl;
+		cout<<"Podaj wspolczynnik a: ";
+	    cin>>a;
+		cout<<"Podaj wspolczynnik p: ";
+	    cin>>p;
+	    cout<<"Podaj wspolczynnik q: ";
+	    cin>>q;	
+	   
+	    b=-2*a*p;
+	    c=(a*pow(p,2))+q;
+	    
+		if(a==0)
+    {
+    	if(b==0)
+    	{
+    		if(c==0)
+    		{
+    			cout<<"Nieskonczenie wiele miejsc zerowych"<<endl;
+			}
+			cout<<"Brak miejsc zerowych"<<endl;
+		}
+		cout<<"Miejsce zerowe x0 wynosi "<<(-c)/b<<endl;
+	} else {
+	
+	double delta=pow(b,2)-(4*a*c);
+    if(delta>0)
+    {
+    	double x1=(-b-sqrt(delta))/(2*a);
+    	double x2=(-b+sqrt(delta))/(2*a);
+    	cout<<"Miejsca zerowe wynosza "<<x1<<" oraz "<<x2<<endl;
+	}
+	else if(delta==0)
+	{
+		double x0=(-b)/(2*a);
+		cout<<"Miejsce zerowe wynosi "<<x0<<endl;
+	}
+	else {
+		cout<<"Nie ma miejsc zerowych"<<endl;
+	}
+		
+}
+		
 	}
 	
 	
